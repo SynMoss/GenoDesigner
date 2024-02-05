4cb88e5f20371d5af9ba65a55e5a775d56304ddc
The synthetic Moss genome project <158453166+SynMoss@users.noreply.github.com>
2024-02-05 16:00:44 +0800

    Add files via upload

---
+++ GenoDesigner-source/client/README.md
@@ -0,0 +1,30 @@
+
+
+```bash
+
+git clone 
+
+
+cd 
+
+
+npm install
+
+
+npm install --registry=https://registry.npmmirror.com
+
+
+npm run dev
+```
+
+http://localhost:80
+
+
+
+```bash
+
+npm run build:stage
+
+
+npm run build:prod
+```
---
+++ GenoDesigner-source/client/vue.config.js
@@ -0,0 +1,139 @@
+'use strict'
+const path = require('path')
+
+function resolve(dir) {
+  return path.join(__dirname, dir)
+}
+
+const CompressionPlugin = require('compression-webpack-plugin')
+
+const name = process.env.VUE_APP_TITLE || ''
+
+const port = process.env.port || process.env.npm_config_port || 80 //
+
+
+module.exports = {
+
+  publicPath: process.env.NODE_ENV === "production" ? "./" : "./",
+
+  outputDir: 'dist',
+
+  assetsDir: 'static',
+
+  lintOnSave: process.env.NODE_ENV === 'development',
+
+  productionSourceMap: false,
+
+  devServer: {
+    host: '0.0.0.0',
+    port: port,
+    open: true,
+    proxy: {
+      // detail: https://cli.vuejs.org/config/#devserver-proxy
+      [process.env.VUE_APP_BASE_API]:
+      {
+        // target: `http://36.133.150.33:3000`,
+        target: `http://localhost:7000`,
+        changeOrigin: true,
+        pathRewrite: {
+          ['^' + process.env.VUE_APP_BASE_API]: ''
+        }
+      },
+      // {
+      //   target: `http://36.133.150.55:3000`,
+      //   changeOrigin: true,
+      //   pathRewrite: {}
+      // }
+    },
+    disableHostCheck: true
+  },
+  css: {
+    loaderOptions: {
+      sass: {
+        sassOptions: { outputStyle: "expanded" }
+      }
+    }
+  },
+  configureWebpack: {
+    name: name,
+    resolve: {
+      alias: {
+        '@': resolve('src')
+      }
+    },
+    plugins: [
+
+      new CompressionPlugin({
+        cache: false,
+        test: /\.(js|css|html)?$/i,
+        filename: '[path].gz[query]',
+        algorithm: 'gzip',
+        minRatio: 0.8
+      })
+    ],
+  },
+  chainWebpack(config) {
+    config.plugins.delete('preload') // TODO: need test
+    config.plugins.delete('prefetch') // TODO: need test
+
+    // set svg-sprite-loader
+    config.module
+      .rule('svg')
+      .exclude.add(resolve('src/assets/icons'))
+      .end()
+    config.module
+      .rule('icons')
+      .test(/\.svg$/)
+      .include.add(resolve('src/assets/icons'))
+      .end()
+      .use('svg-sprite-loader')
+      .loader('svg-sprite-loader')
+      .options({
+        symbolId: 'icon-[name]'
+      })
+      .end()
+
+    config
+      .when(process.env.NODE_ENV !== 'development',
+        config => {
+          config
+            .plugin('ScriptExtHtmlWebpackPlugin')
+            .after('html')
+            .use('script-ext-html-webpack-plugin', [{
+            // `runtime` must same as runtimeChunk name. default is `runtime`
+              inline: /runtime\..*\.js$/
+            }])
+            .end()
+          config
+            .optimization.splitChunks({
+              chunks: 'all',
+              cacheGroups: {
+                libs: {
+                  name: 'chunk-libs',
+                  test: /[\\/]node_modules[\\/]/,
+                  priority: 10,
+                  chunks: 'initial' // only package third parties that are initially dependent
+                },
+                elementUI: {
+                  name: 'chunk-elementUI', // split elementUI into a single package
+                  priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
+                  test: /[\\/]node_modules[\\/]_?element-ui(.*)/ // in order to adapt to cnpm
+                },
+                commons: {
+                  name: 'chunk-commons',
+                  test: resolve('src/components'), // can customize your rules
+                  minChunks: 3, //  minimum common number
+                  priority: 5,
+                  reuseExistingChunk: true
+                }
+              }
+            })
+          config.optimization.runtimeChunk('single'),
+          {
+             from: path.resolve(__dirname, './public/robots.txt'),
+             to: './'
+          }
+        }
+      )
+  }
+}
